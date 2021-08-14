826a76fb3adc7b985ff20f3f6726094eefa44df3
Adrian Aguilar <Tygor23@gmail.com>
2021-08-14 14:47:53 -0700

    setup json server and fetching data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -2,29 +2,22 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState ([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState (null);
 
-    const [name, setName] = useState('Mario');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
-        useEffect(() => {
-            console.log('use effect ran');
-            console.log(name);
-        },[name]);
+         useEffect(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                 return  res.json();
+                })
+                .then((data) => {
+                    setBlogs(data)
+                });
+        },[]);
 
       return (
         <div className="home">
-       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-       <button onClick={() => setName('Luigi')}>Change Name</button>
-       <p>{ name }</p>
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+
         </div>
     );
 }
