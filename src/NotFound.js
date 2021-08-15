0aaf73902e8acb2a1bc69f2b22a457e10466d234
Adrian Aguilar <Tygor23@gmail.com>
2021-08-15 14:29:13 -0700

    changed Link text

--- src/NotFound.js
+++ src/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
         <div className="not-found">
             <h2>Sorry</h2>
             <p>That Page Cannot be Found</p>
-            <Link to='/'>Back to the Homepage...</Link>
+            <Link to='/'>Click Here to Return to Homepage...</Link>
         </div>
     );
 }
